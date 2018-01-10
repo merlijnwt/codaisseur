889a506a03a999100178cdbb492531c862130880
merlijnwt <merlijn@wetransfer.com>
2018-01-10 14:57:11 +0100

    Add ingredients

--- spanish_omelette.rb
+++ spanish_omelette.rb
@@ -1,9 +1,21 @@
 def print_divider
     puts "*" * 40
     puts "\n"
-  end
+end
   
-  def print_progress_bar
+def print_progress_bar
     3.times { sleep 0.5; print "." }
     puts "\n"
-  end
+end
+
+NUM_POTATOES = 5
+NUM_EGGS = 6
+
+ingredients = [
+  { name: 'potatoes', quantity: NUM_POTATOES },
+  { name: 'eggs', quantity: NUM_EGGS },
+  { name: 'onion', quantity: 1 },
+  { name: 'oil', quantity: 'some' },
+  { name: 'salt', quantity: 'some' },
+]
+
