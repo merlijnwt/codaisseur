a292553469b09c40f213818921781c26de21e57a
merlijnwt <merlijn@wetransfer.com>
2018-01-10 15:27:24 +0100

    Ask if user is ready for next step

--- spanish_omelette.rb
+++ spanish_omelette.rb
@@ -110,3 +110,10 @@ steps.each_with_index do |step, index|
       print_progress_bar
     end
   end
+
+  def ask_if_ready(step, index)
+    puts "Are you ready for step #{index + 1}?\n(#{step[:description]})"
+    answer = gets.chomp
+  
+    answer.upcase == 'Y'
+  end
